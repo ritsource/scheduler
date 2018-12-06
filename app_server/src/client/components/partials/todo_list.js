6a8d345afdeee8196e721456d5c4ce92f4db4d07
Ritwik <ritwiksahaofficial@gmail.com>
2018-12-06 12:34:48 +0530

    Added New Event Adder Component

--- app_server/src/client/components/partials/todo_list.js
+++ app_server/src/client/components/partials/todo_list.js
@@ -7,7 +7,8 @@ class TodoListComp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      listTitle: this.props.listTitle || ''
+      listTitle: this.props.listTitle || '',
+      newEventTitle: ''
     }
   }
 
@@ -21,36 +22,64 @@ class TodoListComp extends React.Component {
     const showFormButton = (this.props.listTitle !== this.state.listTitle && this.state.listTitle !== '');
     return (
       <div className='todo-list-000'>
-        <div className='todo-list-001-header'>
-          <form onSubmit={(e) => {
-            e.preventDefault();
-          }}>
-            <input
-              name='listname'
-              autoComplete='off'
-              className={
-                `${(this.state.listTitle === '') && 'list-003-invalid-input'} ${!showFormButton && 'list-003-w-se-g-input'}`
-              }
-              value={this.state.listTitle}
-              onChange={(e) => {
-                this.setState({ listTitle: e.target.value });
-              }}
-            />
-            {showFormButton && (<button type='submit'>Save</button>)}
-          </form>
-          <button className='list-002-trash-button'><i class="far fa-trash-alt"></i></button>
-        </div>
-        {/* <div style={{ background: 'white' }} className='todo-list-001-dropzone' id={0}></div> */}
-        {this.props.events.map((event, i) => (
-          <React.Fragment>
-            <Link to={`/todo?group=${this.props.active_groupId}&event=${event._id}`} onClick={() => {
-              // this.props.changeEventId(event._id);
+        <div className='todo-list-001-content'>
+          <div className='list-002-header'>
+            <form onSubmit={(e) => {
+              e.preventDefault();
             }}>
-              <TodoListItem key={i} event={event} />
-            </Link>
-            {/* <div className='todo-list-001-dropzone' id={i + 1}></div> */}
-          </React.Fragment>
-        ))}
+              <input
+                name='listname'
+                autoComplete='off'
+                className={
+                  `${(this.state.listTitle === '') && 'list-004-invalid-input'} ${!showFormButton && 'list-004-w-se-g-input'}`
+                }
+                value={this.state.listTitle}
+                onChange={(e) => {
+                  this.setState({ listTitle: e.target.value });
+                }}
+              />
+              {showFormButton && (<button type='submit'>Save</button>)}
+            </form>
+            <button className='list-003-trash-button'><i class="far fa-trash-alt"></i></button>
+          </div>
+
+          {/* <div style={{ background: 'white' }} className='todo-list-001-dropzone' id={0}></div> */}
+          <div className='todo-list-002-the-list'>
+            {[
+              ...this.props.events,
+              ...this.props.events,
+              ...this.props.events,
+            ].map((event, i) => (
+              <React.Fragment>
+                <Link to={`/todo?group=${this.props.active_groupId}&event=${event._id}`} onClick={() => {
+                  // this.props.changeEventId(event._id);
+                }}>
+                  <TodoListItem key={i} event={event} />
+                </Link>
+                {/* <div className='todo-list-001-dropzone' id={i + 1}></div> */}
+              </React.Fragment>
+            ))}
+          </div>
+        </div>
+
+        <form className='todo-list-001-new-task-form' onSubmit={(e) => {
+          e.preventDefault();
+        }}>
+          {/* <div className='todo-list-002-form-add-indicator'>
+            <i class="fas fa-plus"></i>
+          </div> */}
+          <input
+            name='newEventTitle'
+            autoComplete='off'
+            className=''
+            placeholder='+ Add a Task'
+            value={this.state.newEventTitle}
+            onChange={(e) => {
+              this.setState({ newEventTitle: e.target.value });
+            }}
+          />
+          {this.state.newEventTitle !== '' && (<button type='submit'>Add</button>)}
+        </form>
       </div>
     )
   }
--- app_server/src/client/components/partials/todo_list_item.js
+++ app_server/src/client/components/partials/todo_list_item.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const TodoListItem = (props) => (
   <div className={`todo-list-item-000 ${props.active && 'todo-list-item-000-active'}`}>
-    {/* <div className='sidebar-item-001-hamburger'>
-      <div></div><div></div><div></div>
-    </div> */}
-    <p>{props.event.title}</p>
+    <div className={`list-item-001-indicator ${props.event._isDone && 'indicator-_isDone'}`}>
+      {props.event._isDone ? (
+        <i class="fas fa-check"></i>
+      ) : (
+        <i class="fas fa-check"></i>
+      )}
+    </div>
+    <p className={props.event._isDone && 'list-item-001-p-_isDone'}>{props.event.title}</p>
   </div>
 );
 
