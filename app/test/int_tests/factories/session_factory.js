d7f5c3122835ea9945975852d5a9556d74238ffa
Ritwik Saha <ritwiksahaofficial@gmail.com>
2019-02-03 05:45:48 +0530

    A Small Fix

--- app/test/int_tests/factories/session_factory.js
+++ app/test/int_tests/factories/session_factory.js
@@ -3,9 +3,9 @@ import Keygrip from 'keygrip';
 
 import keys from '../../../../api/src/config/keys';
 
-export default (userId) => {
+export default (user) => {
   const Buffer = safeBuffer.Buffer;
-  const sessionObj = { passport: { user: userId } };
+  const sessionObj = { passport: { user: user._id.toString() } };
   const session = Buffer.from(JSON.stringify(sessionObj)).toString('base64');
 
   const keygrip = new Keygrip([ keys.cookie_key ]);
