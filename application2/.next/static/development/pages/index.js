e12932389654dac949edbc472bce877b7a7715a3
Ritwik Saha <ritwiksahaofficial@gmail.com>
2019-02-22 22:47:19 +0530

    Added Next.js Server

---
+++ application2/.next/static/development/pages/index.js
@@ -0,0 +1,89 @@
+((window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static/development/pages/index.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2Fhome%2Fritwik310%2FDesktop%2Fmy-calendar%2Fapplication2%2Fpages%2Findex.js!./":
+/*!******************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2Fhome%2Fritwik310%2FDesktop%2Fmy-calendar%2Fapplication2%2Fpages%2Findex.js ***!
+  \******************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/", function() {
+      var page = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/index.js */ "./pages/index.js", function() {
+          if(!next.router.components["/"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/index.js */ "./pages/index.js")
+          next.router.update("/", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_512e962586c18877b7c9 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_512e962586c18877b7c9 */ "dll-reference dll_512e962586c18877b7c9"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _src_styles_app_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../src/styles/app.scss */ "./src/styles/app.scss");
+/* harmony import */ var _src_styles_app_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_src_styles_app_scss__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/home/ritwik310/Desktop/my-calendar/application2/pages/index.js";
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "Home Page");
+});
+
+/***/ }),
+
+/***/ 1:
+/*!**********************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2F&absolutePagePath=%2Fhome%2Fritwik310%2FDesktop%2Fmy-calendar%2Fapplication2%2Fpages%2Findex.js ***!
+  \**********************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2F&absolutePagePath=%2Fhome%2Fritwik310%2FDesktop%2Fmy-calendar%2Fapplication2%2Fpages%2Findex.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F&absolutePagePath=%2Fhome%2Fritwik310%2FDesktop%2Fmy-calendar%2Fapplication2%2Fpages%2Findex.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_512e962586c18877b7c9":
+/*!*******************************************!*\
+  !*** external "dll_512e962586c18877b7c9" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_512e962586c18877b7c9;
+
+/***/ })
+
+},[[1,"static/runtime/webpack.js","styles"]]]));;
+//# sourceMappingURL=index.js.map
---
+++ application2/pages/index.js
@@ -0,0 +1,3 @@
+import '../src/styles/app.scss';
+
+export default () => <div>Home Page</div>;
