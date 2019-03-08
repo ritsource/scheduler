bbea40ce05f918681f2f7d7d4c3db2132215c488
Ritwik Saha <ritwiksahaofficial@gmail.com>
2019-03-08 05:37:35 +0530

    Found a way around to ignore the Bug (Component was not rerendering on state update)

--- client/src/apps/_common/components/EventDetails.js
+++ client/src/apps/_common/components/EventDetails.js
@@ -27,15 +27,119 @@ const EventDetails = (props) => {
 	const { event, groups, contextSteps, setContextSteps, client, pathName, hex_color, closeEventDetails } = props;
 
 	const [ steps, setSteps ] = useState([]);
+	const [ reRenderMnky, setReRenderMnky ] = useState(true);
 
 	const gqlRefetchQueries = pathName === 'todo' ? [ 'readAllGroups' ] : [ 'readGroupsOnCalendar' ];
 
+	// useEffect(
+	// 	() => {
+	// 		// readAndSaveStepsData();
+	// 		if (steps.length > 0) {
+	// 			setContextSteps([ ...contextSteps.filter(({ _event }) => _event !== event._id), ...steps ]);
+	// 		}
+	// 	},
+	// 	[ steps ]
+	// );
+
 	useEffect(
 		() => {
-			// readAndSaveStepsData();
-			if (steps.length > 0) {
-				setContextSteps([ ...contextSteps.filter(({ _event }) => _event !== event._id), ...steps ]);
-			}
+			client
+				.query({
+					query: FETCH_STEPS_BY_EVENT,
+					variables: { eventId: event._id }
+				})
+				.then(({ data }) => {
+					console.log(data.readStepsByEvent);
+
+					setSteps(data.readStepsByEvent);
+				});
+
+			return () => setSteps([]);
+		},
+		[ event ]
+	);
+
+	// Handle New Step Submit
+	const onStepSubmit = (title) => {
+		client
+			.mutate({
+				mutation: ADD_NEW_STEP,
+				variables: { title, _event: event._id },
+				refetchQueries: [ 'readStepsByEvent' ],
+				awaitRefetchQueries: true
+			})
+			.then(() => {
+				scrollToBottom('.EventDetails-The-List-01');
+			});
+	};
+
+	// Handle Event Edit
+	const handleStepEdit = async ({ stepId, title }) => {
+		await client.mutate({
+			mutation: EDIT_STEP_BY_ID,
+			variables: { stepId, title },
+			refetchQueries: [ 'readStepsByEvent' ]
+		});
+	};
+
+	const handleStepDelete = async (stepId) => {
+		await client.mutate({
+			mutation: DELETE_STEP,
+			variables: { stepId },
+			refetchQueries: [ 'readStepsByEvent' ]
+		});
+	};
+
+	const stepDoneHandeler = async (stepId, boolean) => {
+		await client.mutate({
+			mutation: boolean ? EDIT_STEP_TO_DONE : EDIT_STEP_TO_NOT_DONE,
+			variables: { stepId },
+			refetchQueries: [ 'readStepsByEvent' ]
+		});
+	};
+
+	const onDragEnd = (result) => {
+		if (result.source.index === result.destination.index) return;
+
+		const tempSteps = [ ...steps ];
+
+		const fromIndex = result.source.index;
+		const toIndex = result.destination.index;
+
+		const movedSteps =
+			fromIndex < toIndex
+				? tempSteps.slice(fromIndex + 1, toIndex + 1).map(({ _id }) => _id)
+				: tempSteps.slice(toIndex, fromIndex).map(({ _id }) => _id);
+
+		client
+			.mutate({
+				mutation: REARRANGE_STEPS,
+				variables: {
+					focusedStep: result.draggableId,
+					fromRank: tempSteps[fromIndex]._rank,
+					toRank: tempSteps[toIndex]._rank,
+					movedSteps: movedSteps
+				},
+				refetchQueries: [ 'readStepsByEvent' ],
+				awaitRefetchQueries: true
+			})
+			.then(() => {});
+
+		const focusedStep = tempSteps.splice(fromIndex, 1)[0];
+		tempSteps.splice(toIndex, 0, focusedStep);
+
+		// console.log('tempSteps', tempSteps);
+
+		console.log('LOL');
+		setReRenderMnky(false);
+		setSteps(tempSteps);
+	};
+
+	useEffect(
+		() => {
+			// FIXME: couldn't fix the bug but feigured a way out (something is wrong, file a issue maybe)
+			window.getSteps = () => steps;
+			setReRenderMnky(true);
 		},
 		[ steps ]
 	);
@@ -78,71 +182,6 @@ const EventDetails = (props) => {
 		});
 	};
 
-	// Handle New Step Submit
-	const onStepSubmit = (title) => {
-		client
-			.mutate({
-				mutation: ADD_NEW_STEP,
-				variables: { title, _event: event._id },
-				refetchQueries: [ 'readStepsByEvent' ],
-				awaitRefetchQueries: true
-			})
-			.then(() => {
-				scrollToBottom('.EventDetails-The-List-01');
-			});
-	};
-
-	// Handle Event Edit
-	const handleStepEdit = async ({ stepId, title }) => {
-		await client.mutate({
-			mutation: EDIT_STEP_BY_ID,
-			variables: { stepId, title },
-			refetchQueries: [ 'readStepsByEvent' ]
-		});
-	};
-
-	const handleStepDelete = async (stepId) => {
-		await client.mutate({
-			mutation: DELETE_STEP,
-			variables: { stepId },
-			refetchQueries: [ 'readStepsByEvent' ]
-		});
-	};
-
-	const stepDoneHandeler = async (stepId, boolean) => {
-		await client.mutate({
-			mutation: boolean ? EDIT_STEP_TO_DONE : EDIT_STEP_TO_NOT_DONE,
-			variables: { stepId },
-			refetchQueries: [ 'readStepsByEvent' ]
-		});
-	};
-
-	const onDragEnd = async (result) => {
-		if (result.source.index === result.destination.index) return;
-
-		const tempSteps = [ ...steps ];
-
-		const fromIndex = result.source.index;
-		const toIndex = result.destination.index;
-
-		const movedSteps =
-			fromIndex < toIndex
-				? tempSteps.slice(fromIndex + 1, toIndex + 1).map(({ _id }) => _id)
-				: tempSteps.slice(toIndex, fromIndex).map(({ _id }) => _id);
-
-		await client.mutate({
-			mutation: REARRANGE_STEPS,
-			variables: {
-				focusedStep: result.draggableId,
-				fromRank: tempSteps[fromIndex]._rank,
-				toRank: tempSteps[toIndex]._rank,
-				movedSteps: movedSteps
-			},
-			refetchQueries: [ 'readStepsByEvent' ],
-			awaitRefetchQueries: true
-		});
-	};
-
 	return (
 		<div className="EventDetails-c-00">
 			<EventDetailsHeader
@@ -154,47 +193,29 @@ const EventDetails = (props) => {
 			<DragDropContext onDragEnd={onDragEnd}>
 				<Droppable droppableId={event._id} type="STEP_DND">
 					{(provided) => (
-						<Query query={FETCH_STEPS_BY_EVENT} variables={{ eventId: event._id }}>
-							{({ loading, error, data, refetch }) => {
-								if (error) {
-									return console.log('Error', error);
-								}
+						<div className="EventDetails-The-List-01" ref={provided.innerRef} {...provided.droppableProps}>
+							{reRenderMnky &&
+								steps.map((step, i) => {
+									// if (i === 0) console.log('XXX');
+									// console.log(i, step.title);
+									// console.log(steps);
 
-								if (data.readStepsByEvent) {
-									console.log('data.readStepsByEvent');
-									setSteps(data.readStepsByEvent);
-								}
+									// FIXME: A Bug Exist Here!
+									// "contextSteps" isn't getting rerendered after Drag&Drop ends
 
-								return (
-									<div
-										className="EventDetails-The-List-01"
-										ref={provided.innerRef}
-										{...provided.droppableProps}
-									>
-										{data.readStepsByEvent &&
-											data.readStepsByEvent.map((eachStep, i) => {
-												if (i === 0) console.log('XXX');
-												console.log(i, eachStep.title);
-
-												// FIXME: A Bug Exist Here!
-												// "contextSteps" isn't getting rerendered after Drag&Drop ends
-
-												return (
-													<EventDetailsItem
-														key={i}
-														index={i}
-														step={eachStep}
-														hex_color={hex_color}
-														handleStepEdit={handleStepEdit}
-														handleStepDelete={handleStepDelete}
-														stepDoneHandeler={stepDoneHandeler}
-													/>
-												);
-											})}
-									</div>
-								);
-							}}
-						</Query>
+									return (
+										<EventDetailsItem
+											key={i}
+											index={i}
+											step={step}
+											hex_color={hex_color}
+											handleStepEdit={handleStepEdit}
+											handleStepDelete={handleStepDelete}
+											stepDoneHandeler={stepDoneHandeler}
+										/>
+									);
+								})}
+						</div>
 					)}
 				</Droppable>
 			</DragDropContext>
