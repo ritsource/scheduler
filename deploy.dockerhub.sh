9286b1b03b254b9feebc2d39943852a302642e37
Ritwik Saha <ritwiksahaofficial@gmail.com>
2019-04-11 01:47:00 +0530

    Troubleshooting, Pushing Nignx Image

--- deploy.dockerhub.sh
+++ deploy.dockerhub.sh
@@ -1,8 +1,11 @@
 docker build -t ritwik310/scheduler-client:latest -t ritwik310/scheduler-client:$GIT_SHA -f ./client/Dockerfile ./client
 docker build -t ritwik310/scheduler-server:latest -t ritwik310/scheduler-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t ritwik310/scheduler-nginx:latest -t ritwik310/scheduler-nginx:$GIT_SHA -f ./nginx/Dockerfile ./nginx
 
 docker push ritwik310/scheduler-client:latest
 docker push ritwik310/scheduler-server:latest
+docker push ritwik310/scheduler-nginx:latest
 
 docker push ritwik310/scheduler-client:$GIT_SHA
 docker push ritwik310/scheduler-server:$GIT_SHA
+docker push ritwik310/scheduler-nginx:$GIT_SHA
